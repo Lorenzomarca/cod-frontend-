6639a2cb64bef426a0f9d7c937a911173b7e74ba
Lorenzomarca <proplayerdocs2@gmail.com>
2026-09-22 17:05:48 -0300

    02-começado

---
+++ 02-studio alfa/src/Header.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function Header() {
+  return (
+    <header>
+      <div className="logoD">
+        <img
+          className="logoS"
+          src="https://static.wikia.nocookie.net/bluelock/images/b/b0/Real_Madrid.PNG/revision/latest?cb=20240122040731&path-prefix=es"
+          alt=""
+        />
+        <span>Studio Alfa</span>
+      </div>
+      <nav>
+        <ul className="navigation">
+          <li className="menu">
+            <a href="#">Início</a>
+          </li>
+          <li className="menu">
+            <a href="#">Serviços</a>
+          </li>
+          <li className="menu">
+            <a href="#">Sobre</a>
+          </li>
+          <li className="menu">
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
---
+++ 02-studio alfa/src/Principal.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function Principal() {
+  return (
+    <main>
+      <section className="section-1">
+      <h1 className="Titulo">Criamos sites que funcionam</h1>
+      <p className="Descrição">
+        Layouts responsivos, rápidos e acessíveis para o seu <br />
+        negócio crescer na web.
+      </p>
+
+      <div className="botoes">
+        <button className="btn-orçamento">Peça um orçamento</button>
+        <button className="btn-portifolio">Ver portifolio</button>
+      </div>
+      </section>
+      <section className="section-2">
+        
+      </section>
+    </main>
+  );
+}
+export default Principal;
